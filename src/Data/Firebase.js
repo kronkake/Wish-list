14e6aa75de32371b5e72e584a041a9e5c13e0152
Jonas Dahlen Svendsen <jonas.svendsen@bouvet.no>
2018-04-25 10:52:02 +0200

    Firebase offline persistance

--- src/Data/Firebase.js
+++ src/Data/Firebase.js
@@ -13,6 +13,22 @@ const config = {
 Firebase.initializeApp(config)
 const FirebaseUserCreation = Firebase.initializeApp(config, 'UserCreation')
 
+Firebase.firestore().enablePersistence()
+  .then(function() {
+
+  })
+  .catch(function(err) {
+      if (err.code === 'failed-precondition') {
+          // Multiple tabs open, persistence can only be enabled
+          // in one tab at a a time.
+          // ...
+      } else if (err.code === 'unimplemented') {
+          // The current browser does not support all of the
+          // features required to enable persistence
+          // ...
+      }
+  });
+
 const Firestore = Firebase.firestore()
 
 const AdminUserId = 'eWYqiOzNNOcJVTVzezHh22FwE703'
